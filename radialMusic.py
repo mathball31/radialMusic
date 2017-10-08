6b788a6864c43ae1c2d8297368ce9f785966d781
Dirk Lamb <dirk.lamb31@gmail.com>
2017-10-07 20:25:09 -0600

    Initial commit

---
+++ radialMusic.py
@@ -0,0 +1,17 @@
+"""
+This is a script that will hopefully eventually be fleshed into something 
+that can convert an image into sound. Currently, it will only take in a Polar coordinate function,
+and convert it into a number.
+
+Hopeful final program flow:
+	Read image
+	Convert to function
+	Convert Function to number
+	Convert number to sound
+
+This script will at first only cover the conversion of Function to number
+
+
+Author: Dirk Lamb
+Date: 2017-10-7
+"""
